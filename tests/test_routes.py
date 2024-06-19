0b743e7d299af62189a98887c2bbbf57a59d1b00
Patrick Ruch <patrick.ruch@gmail.com>
2024-06-19 05:53:43 -0400

    wip: test failing

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -131,3 +131,12 @@ class TestAccountService(TestCase):
         account_list = response.get_json()
         self.assertEqual(len(account_list), 5)
 
+    def test_get_account(self):
+        """It should Read a single Account"""
+        account = self._create_accounts(1)[0]
+        response = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["name"], account.name)
