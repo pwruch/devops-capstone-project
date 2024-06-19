ef092f1d0129da0ba0a92962c36fce8c21e304bb
pwruch <117715603+pwruch@users.noreply.github.com>
2024-06-19 15:22:56 +0200

    Read account (#11)
    * wip: test failing
    
    * work in progress - failing test
    
    * feat: get an account
    
    ---------
    
    Co-authored-by: Patrick Ruch <ruc@zurich.ibm.com>

--- service/routes.py
+++ service/routes.py
@@ -78,8 +78,24 @@ def list_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
-# ... place you code here to READ an account ...
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def get_account(account_id):
+    """
+    Retrieve a single Account
 
+    This endpoint will return an account based on it's id
+    """
+
+    app.logger.info(
+        "Request to Retrieve an account with id [%s]", account_id)
+    account = Account.find(account_id)
+
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id '{account_id}' was not found.")
+
+    app.logger.info("Returning account: %s", account.name)
+
+    return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
