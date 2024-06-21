d67034c4ce031201df855642592663817610ce48
pwruch <117715603+pwruch@users.noreply.github.com>
2024-06-21 11:35:25 +0200

    Cd pipeline (#21)
    * added link task
    
    * added nose task
    
    * added test pipeline task
    
    * added build task
    
    * added deploy task
    
    * fixes in progress
    
    * fixes in progress
    
    * fixes in progress
    
    * fixes in progress
    
    * fixes in progress
    
    * fixes in progress
    
    * final cd pipeline

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -24,6 +24,8 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
+
+
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
@@ -76,12 +78,10 @@ class TestAccountService(TestCase):
     #  A C C O U N T   T E S T   C A S E S
     ######################################################################
 
-
     def test_index(self):
         """It should get 200_OK from the Home Page"""
         response = self.client.get("/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
 
     def test_health(self):
         """It should be healthy"""
@@ -89,7 +89,6 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, 200)
         data = resp.get_json()
         self.assertEqual(data["status"], "OK")
-
 
     def test_create_account(self):
         """It should Create a new Account"""
@@ -113,12 +112,10 @@ class TestAccountService(TestCase):
         self.assertEqual(new_account["phone_number"], account.phone_number)
         self.assertEqual(new_account["date_joined"], str(account.date_joined))
 
-
     def test_bad_request(self):
         """It should not Create an Account when sending the wrong data"""
         response = self.client.post(BASE_URL, json={"name": "not enough data"})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
 
     def test_unsupported_media_type(self):
         """It should not Create an Account when sending the wrong media type"""
@@ -130,7 +127,6 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-
     def test_list_all_accounts(self):
         """It should List all Accounts"""
         self._create_accounts(5)        # create 5 test accounts
@@ -138,7 +134,6 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         account_list = response.get_json()
         self.assertEqual(len(account_list), 5)
-
 
     def test_get_account(self):
         """It should Read a single Account"""
@@ -150,19 +145,16 @@ class TestAccountService(TestCase):
         data = response.get_json()
         self.assertEqual(data["name"], account.name)
 
-
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
-
 
     def test_delete_account(self):
         """It should Delete an Account"""
         account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
-
 
     def test_update_account(self):
         """It should Update an existing Account"""
@@ -179,13 +171,11 @@ class TestAccountService(TestCase):
         updated_account = resp.get_json()
         self.assertEqual(updated_account["name"], "Something Known")
 
-
     def test_method_not_allowed(self):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
-    
     def test_https_environment(self):
         """It should return the correct security headers"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
@@ -201,6 +191,6 @@ class TestAccountService(TestCase):
 
     def test_cors_policies(self):
         """It should verify cross-origin resource sharing"""
-        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)         
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
